07b0437c09e41fb2862b4af2530b8f68dd5540bb
HiperbolickiParaboloid <mirko.mljnc@gmail.com>
2019-08-19 13:01:54 +0200

    update

--- scraper.py
+++ scraper.py
@@ -1,22 +1,31 @@
 import scrape_links
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask
 
-num = 100
+app = Flask(__name__)
 
-link_part_one = "https://www.realitica.com/?cur_page="
-link_part_two = "&for=DuziNajam&pZpa=Crna+Gora&pState=Crna+Gora&type%5B%5D=&lng=hr"
+#num = 100
 
-while 1:
-    link = link_part_one + str(num) + link_part_two
-    source = requests.get(link).text
-    soup = BeautifulSoup(source, "lxml")
-    try:
-        indicator = soup.find("div", id = "content_holder2").h3.text
-        if indicator == "Oglas Nije Pronađen":
-            print ("Zavrseno!")
-            break
-    except Exception as e:
-        pass
-    scrape_links.scrape_links(link)
-    num = num + 1
+@app.route("/")
+def scraper():
+    num = 100
+    link_part_one = "https://www.realitica.com/?cur_page="
+    link_part_two = "&for=DuziNajam&pZpa=Crna+Gora&pState=Crna+Gora&type%5B%5D=&lng=hr"
+
+    while 1:
+        link = link_part_one + str(num) + link_part_two
+        source = requests.get(link).text
+        soup = BeautifulSoup(source, "lxml")
+        try:
+            indicator = soup.find("div", id = "content_holder2").h3.text
+            if indicator == "Oglas Nije Pronađen":
+                print ("Zavrseno!")
+                break
+        except Exception as e:
+            pass
+        scrape_links.scrape_links(link)
+        num = num + 1
+    return "ok"
+
+app.run(port=5000, debug=True)
