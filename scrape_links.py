974612339653c65b4fec8746635f627fb9f14918
HiperbolickiParaboloid <mirko.mljnc@gmail.com>
2019-08-20 18:32:13 +0200

    Updated scrape_links

--- scrape_links.py
+++ scrape_links.py
@@ -8,6 +8,21 @@ def scrape_links(page_link):
         lista = link.split("listing/")
         int_ajdi = int(lista[1])
         return int_ajdi
+    
+    def scrape_date(link):
+        source = requests.get(link).text
+        soup = BeautifulSoup(source, "lxml")
+        listing_body = soup.find("div", id = "listing_body")
+        cont = listing_body.div.extract().stripped_strings
+        tekst = listing_body.text
+        x = tekst.split("\n")
+        for i in x:
+            if i.count("Zadnja Promjena") > 0:
+                indeks = x.index(i)
+        zadnja_promjena = x[indeks].split(": ")[1]
+        zadnja_promjena = datetime.datetime.strptime(zadnja_promjena, "%d %b, %Y")
+        return zadnja_promjena
+
 
     myclient = pymongo.MongoClient("mongodb+srv://mirko:admin@scrapermirko-3rjq2.mongodb.net/test?retryWrites=true&w=majority")
     mydb = myclient["Realitica"]
@@ -30,9 +45,15 @@ def scrape_links(page_link):
     for link in lista_linkova:
         idd = ajdi(link)
         indikator = mycol.find_one({"Oglas_Broj": idd})
-        if not indikator:
+        if indikator:
+            zadnja_promjena = scrape_date(link)
+            if zadnja_promjena == indikator.get("zadnja promjena"):
+                print("Already scraped "+link)
+            else:
+                stan = scrape_page.scrape_page(link)
+                mycol.insert_one(stan)
+                print(stan)
+        else:
             stan = scrape_page.scrape_page(link)
             mycol.insert_one(stan)
-            print(scrape_page.scrape_page(link))
-        else:
-            print("Already scraped "+link)
+            print(stan)
